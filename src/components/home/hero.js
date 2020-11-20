d98965c5da8fe7a6190ee3b57ed1a36c9ba73100
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-11-21 00:36:00 +0800

    initialized gatsby

---
+++ src/components/home/hero.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Layout from "../layout"
+
+const Hero = () => <div className="background">sss</div>
+
+export default Hero
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Hero from "../components/home/hero"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Hero />
+  </Layout>
+)
+
+export default IndexPage
